c3ebc4f9aeb3a31ea8d6af32c64abc3706c9c594
George-Jenkins <george.l.jenkins@live.com>
2015-01-19 03:00:21 -0500

    remove

--- js/test-notification.js
+++ js/test-notification.js
@@ -8,44 +8,7 @@ var test = 	navigator.platform
 	
 alert(test)	
 	
-pushNotification = window.plugins.pushNotification;
-  
-alert(test)  
 
-var platform = 	navigator.platform
-
-if ( platform == 'android' || platform == 'Android' || platform == "amazon-fireos" ){
-    pushNotification.register(
-    successHandler,
-    errorHandler,
-    {
-        "senderID":"replace_with_sender_id",
-        "ecb":"onNotification"
-    });
-} else if (platform == 'blackberry10' || platform == 'BlackBerry'){
-    pushNotification.register(
-    successHandler,
-    errorHandler,
-    {
-        invokeTargetId : "replace_with_invoke_target_id",
-        appId: "replace_with_app_id",
-        ppgUrl:"replace_with_ppg_url", //remove for BES pushes
-        ecb: "pushNotificationHandler",
-        simChangeCallback: replace_with_simChange_callback,
-        pushTransportReadyCallback: replace_with_pushTransportReady_callback,
-        launchApplicationOnPush: true
-    });
-} else {
-    pushNotification.register(
-    tokenHandler,
-    errorHandler,
-    {
-        "badge":"true",
-        "sound":"true",
-        "alert":"true",
-        "ecb":"onNotificationAPN"
-    });
-}   
    
    
 }//function 
