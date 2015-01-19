c8bfa63e79653872c1b6ca0ff259a4d46af71902
George-Jenkins <george.l.jenkins@live.com>
2015-01-19 04:17:23 -0500

    p

---
+++ js/test-notification.js
@@ -0,0 +1,11 @@
+var pushNotification;
+
+document.addEventListener("deviceready", function(){
+	
+      alert("123");
+	  
+	  test = navigator.platform
+	  
+	  if(test) alert(test)
+	  
+},true);
