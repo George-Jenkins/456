f5b6e473cd13517c3097644778ec677a7e785e93
George-Jenkins <george.l.jenkins@live.com>
2015-01-19 16:01:37 -0500

    test

--- js/test-notification.js
+++ js/test-notification.js
@@ -2,10 +2,43 @@ var pushNotification;
 
 document.addEventListener("deviceready", function(){
 	
-      alert("123");
+	//get platform
+     var platform = navigator.platform
+	 
+	 pushNotification = window.plugins.pushNotification;
 	  
-	  test = device.platform
-	  
-	  alert(test)
+	 if (platform == 'android' || platform == 'Android' || platform == "amazon-fireos" ){
+    pushNotification.register(
+    successHandler,
+    errorHandler,
+    {
+        "senderID":"replace_with_sender_id",
+        "ecb":"onNotification"
+    });
+} else if (platform == 'blackberry10' || platform == 'BlackBerry'){
+    pushNotification.register(
+    successHandler,
+    errorHandler,
+    {
+        invokeTargetId : "replace_with_invoke_target_id",
+        appId: "replace_with_app_id",
+        ppgUrl:"replace_with_ppg_url", //remove for BES pushes
+        ecb: "pushNotificationHandler",
+        simChangeCallback: replace_with_simChange_callback,
+        pushTransportReadyCallback: replace_with_pushTransportReady_callback,
+        launchApplicationOnPush: true
+    });
+} else {
+	alert('ok')
+    pushNotification.register(
+    tokenHandler,
+    errorHandler,
+    {
+        "badge":"true",
+        "sound":"true",
+        "alert":"true",
+        "ecb":"onNotificationAPN"
+    });
+} 
 	  
 },true);
