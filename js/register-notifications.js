3ba2d61fd205e9018b7514121f88fd9849fba92c
George-Jenkins <george.l.jenkins@live.com>
2015-02-04 03:14:10 -0500

    test7

--- js/register-notifications.js
+++ js/register-notifications.js
@@ -1,6 +1,4 @@
-alert('j')
- var platform = navigator.platform
- alert(platform)
+alert()
 (function(){
 
 document.addEventListener("deviceready", function(){
@@ -21,28 +19,7 @@ var pushNotification;
 	 
 	 pushNotification = window.plugins.pushNotification;
 	  
-	 if (platform == 'android' || platform == 'Android' || platform == "amazon-fireos" ){
-    pushNotification.register(
-    successHandler,
-    errorHandler,
-    {
-        "senderID":"21900408987",
-        "ecb":"onNotification"
-    });
-} else if (platform == 'blackberry10' || platform == 'BlackBerry'){
-    pushNotification.register(
-    successHandler,
-    errorHandler,
-    {
-        invokeTargetId : "replace_with_invoke_target_id",
-        appId: "replace_with_app_id",
-        ppgUrl:"replace_with_ppg_url", //remove for BES pushes
-        ecb: "pushNotificationHandler",
-        simChangeCallback: replace_with_simChange_callback,
-        pushTransportReadyCallback: replace_with_pushTransportReady_callback,
-        launchApplicationOnPush: true
-    });
-} else {
+	if (platform == 'ios' || platform == 'iPhone'){
     pushNotification.register(
     tokenHandler,
     errorHandler,
@@ -51,6 +28,15 @@ var pushNotification;
         "sound":"true",
         "alert":"true",
         "ecb":"onNotificationAPN"
+    });
+} else {
+   
+	pushNotification.register(
+    successHandler,
+    errorHandler,
+    {
+        "senderID":"21900408987",
+        "ecb":"onNotification"
     });
 } 
 
