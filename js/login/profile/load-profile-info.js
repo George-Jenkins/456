1cb6fac6c3d689be28721ad5a8796b64fb9b3392
George-Jenkins <george.l.jenkins@live.com>
2015-02-12 00:08:46 -0500

    test21

--- js/login/profile/load-profile-info.js
+++ js/login/profile/load-profile-info.js
@@ -14,33 +14,7 @@ else postPath = '';
 		
 		if(data){
 		
-		//load profile pic
-		$('#profile-pic-div').css('background-image','url('+postPath+data.profile_pic+')');
-	
-		//load profile pic
-		$('body').addClass('profile-background').css('background-image','url('+postPath+data.profile_background+')');
-	
-		if(data.cover_path){
-			$('#cover-photo-div').css('background-image','url('+postPath+data.cover_path+')').show()
-			$('#add-cover-span').hide()
-			$('#delete-cover-span').show()
-			$('#profile-title').html('Profile')
-			
-		}
-		else
-		{
-			$('#profile-title').html(data.name)
-		}
-		//load name in profile
-		$('#cover-photo-name').html(data.name)
-			
-		$('#home-town-info').html(data.hometown)
-		$('#going-out-answer').html(data.going_out)
 		
-		if(data.list){	
-			if(postPath) data.list = data.list.replace(/background-image:url\(/g,'background-image:url('+postPath)//if postPath provide external path
-			$('#entourages-list').html(data.list)
-		}//if
 		
 	}//if data
 	
